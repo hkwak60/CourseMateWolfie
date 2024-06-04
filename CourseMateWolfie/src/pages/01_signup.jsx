9575d10c22d7d39029706bdc84cbaa31aa0f5df0
danny010904@gmail.com <114132860+HanseungChoi@users.noreply.github.com>
2024-06-04 17:17:42 +0900

    Updated server

--- CourseMateWolfie/src/pages/01_signup.jsx
+++ CourseMateWolfie/src/pages/01_signup.jsx
@@ -13,6 +13,7 @@ export default function SignUp() {
     setPassword(onChangeValue.target.value);
   };
   const handleSave = (e) => {
+    e.preventDefault();
     if (password.length < 4) {
       alert("Use password of at least 4 characters!");
     } else {
@@ -20,14 +21,18 @@ export default function SignUp() {
         user_name: name,
         user_password: password,
       };
+      console.log("check1");
       axios
-        .post("http://localhost:8000/postAccount", formData, {})
-        .then((response) => {})
+        .post("http://localhost:8000/postAccount", idpw, {})
+        .then((response) => {
+          console.log("New account created!");
+        })
         .catch((error) => {
           console.error("Error:", error);
         });
+      console.log("check2");
       alert("New account created!");
-      window.location.href = "/login";
+      // window.location.href = "/login";
     }
   };
 
