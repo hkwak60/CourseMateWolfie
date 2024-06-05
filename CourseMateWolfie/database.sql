e46151f4f7cd15b01668f69d04fe0a24df0c4007
danny010904@gmail.com <114132860+HanseungChoi@users.noreply.github.com>
2024-06-05 19:50:35 +0900

    Update database.sql

--- CourseMateWolfie/database.sql
+++ CourseMateWolfie/database.sql
@@ -2,8 +2,26 @@ CREATE DATABASE project;
 USE project;
 
 CREATE TABLE users (
-user_name nvarchar(100),
-user_password nvarchar(100)
+	user_name nvarchar(100),
+    user_password nvarchar(100)
 );
 
+CREATE TABLE online_user (
+	user_id int,
+    user_name nvarchar(100),
+    user_password nvarchar(100)
+);
+
+CREATE TABLE course_eval (
+	user_id int,
+	course nvarchar(30),
+    item nvarchar(200),
+    average nvarchar(200),
+    percentage varchar(200)
+);
+
+TRUNCATE TABLE users;
 SELECT * FROM users;
+SELECT * FROM online_user;
+
+SELECT * FROM course_eval;
